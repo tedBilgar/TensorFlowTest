4a3067e63d4752bc131e246fdc5dd6d33c0f91f1
Denis Urakanov <mcquicklyss@gmail.com>
2020-07-22 18:44:16 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import numpy as np
+from random import randint
+from sklearn.utils import shuffle
+from sklearn.preprocessing import MinMaxScaler
+
+train_labels = []
+train_samples = []
+
+for i in range(50):
+    random_younger = randint(13, 64)
+    train_samples.append(random_younger)
+    train_labels.append(1)
+
+    random_older = randint(65, 100)
+    train_samples.append(random_older)
+    train_labels.append(0)
+
+for i in range(1000):
+    random_younger = randint(13, 64)
+    train_samples.append(random_younger)
+    train_labels.append(0)
+
+    random_older = randint(65, 100)
+    train_samples.append(random_older)
+    train_labels.append(1)
+
+for i in train_samples:
+    print(i)
